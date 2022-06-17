5d3249d52611fc9ff974378ea0697e9d31947013
delphine <delphin38@live.fr>
2022-06-17 19:01:46 +0200

    accueil en cours

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -1,10 +1,31 @@
 import React from "react";
 
+//images
+import FleurAccueil from '../images/fleuraccueil.jpg';
+
 
 const Accueil = () =>{
     return(
         <div>
-            accueil
+            <div className="row">
+                <div className="col-6">
+                    <h1 className="h1accueil">Théa and Mom's <br />
+                        Créations </h1>
+                        <p>De l'Amour, de l'Art et des Paillettes !</p>
+                        <p>Bienvenue sur le site de notre Boutique en ligne.</p>
+                        <p>Vous y trouverez des bijoux en Résine et Fleurs Naturelles à des prix abordables. Consultez notre catalogue et profitez de votre shopping avec "Théa and Mom's Créations".</p>
+                        <p>Théa a 6 ans et moi 36 ans (sa maman , Michèle LANDAIS) réalisons de nombreux bijoux et objets du quotidien avec des fleurs locales que nous séchons ensemble (fleurs d’Isère, Ain, Rhône). Certains Objets sont composés par Théa et d'autre par mes soins, ce sera signalé soit en description soit en #HASHTAG Made by THEA si c'est THEA, ou Made By Mom si c'est moi :-)</p>
+                        <p>Bien entendu ma petite Théa va recevoir dans sa tirelire quelques euros sur les ventes où elle a participé au séchage des fleurs, quand elle compose elle même les bijoux/objets en vente ), également quand elle n’a pas participé à mes propres créations car c’est avant tout un DUO Mère Fille.(2%)
+</p>
+<p>Je vous conseille d'aller voir notre page <a href={"https://www.facebook.com/mickaela.land"} target="_blank">Facebook</a> où vous pourrez suivre nos aventures. Contactez-nous pour toute question :-) https://www.facebook.com/TheaAndMom
+</p>
+
+                </div>
+                <div className="col-6">
+                    <img src={FleurAccueil} alt="fleur accueil" width="100%" />
+
+                </div>
+            </div>
         </div>
     )
 }
