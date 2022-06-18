d67e52fd7bb4c769886812b54e00b05438049505
delphine <delphin38@live.fr>
2022-06-18 15:25:11 +0200

    contact en cours

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+//images
+import ImageMom from "../images/mom.jpg";
+
 const Contact = ()=>{
   return (
     <div>
-      Contact
+      <div className='container'>
+        <div className='row'>
+            <div className='col-6'>
+                <h3>Nous contacter</h3>
+                <a href='thea.mom.creation@gmail.com'>thea.mom.creation@gmail.com</a>
+            </div>
+            <div className='col-6'>
+                <img src={ImageMom} alt="mama de thea" />
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
